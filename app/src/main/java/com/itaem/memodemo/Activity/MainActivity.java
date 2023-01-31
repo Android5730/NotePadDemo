54e612ab3aee97b44ef0cdb2d78bb6ab20a94595
Android5730 <577300715@qq.com>
2023-01-31 21:54:49 +0800

    1.31:实现夜间模式

--- app/src/main/java/com/itaem/memodemo/Activity/MainActivity.java
+++ app/src/main/java/com/itaem/memodemo/Activity/MainActivity.java
@@ -1,8 +1,12 @@
 package com.itaem.memodemo.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
@@ -28,11 +32,26 @@ public class MainActivity extends AppCompatActivity {
         // 往容器添加碎片，并提交
         fragmentManager.beginTransaction().add(R.id.container_main,new HomePageFragment()).commit();
 
+
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        int currentNightMode = newConfig.uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:
+                // Night mode is not active, we're using the light theme
+                recreate();
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                // Night mode is active, we're using dark theme
+                recreate();
+                break;
+        }
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_VISIBLE|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);//实现状态栏图标和文字颜色为白色
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);//实现状态栏图标和文字颜色为暗色
-
     }
-
 }
